244004aee23cb81049a0aa735a6ab82f9a4d607b
Katherine <katabeth.ch@gmail.com>
2024-06-17 14:59:29 +0100

    Removed printing in method and method returns string. String is then printied in main

--- JavaBasics/src/main/java/com/sparta/kch/Program.java
+++ JavaBasics/src/main/java/com/sparta/kch/Program.java
@@ -1,32 +1,34 @@
 package com.sparta.kch;
 //Readability, Maintainable, Testability
 /*
-                Logic is wrong, with overlapping boundaries, unnecessary searches and unchanging variable
-                All code should never all exist in the main
-                Each method should only do one thing
-                    e.g. initialising, logic, printing
+    Logic is wrong, with overlapping boundaries, unnecessary searches and unchanging variable
+    All code should never all exist in the main
+    Each method should only do one thing
+        e.g. initialising, logic, printing
 */
 
 
 public class Program {
     public static void main(String[] args) { //entry point (methods)
-        int timeOfDay = 21;
-        //type, name, value
+        int timeOfDay = 12;
+        //TYPE, NAME, VALUE
         //strongly typed language (TYPE must always be declared)
         //statically typed language (TYPE cannot be changed from the declaration)
 
-        getGreeting(timeOfDay);
 
+        System.out.println(getGreeting(timeOfDay));
 
     }
     //Cannot be seen outside of class, -- , doesn't return a value
-    private static void getGreeting(int timeOfDay) {
+    private static String getGreeting(int timeOfDay) {
+        String greeting;
         if (timeOfDay >= 5 && timeOfDay <= 12) { //always a boolean
-            System.out.println("Good morning!");
+            greeting = "Good morning!";
         } else if (timeOfDay >= 12 && timeOfDay <= 18) {
-            System.out.println("Good afternoon!");
+            greeting = "Good afternoon!";
         } else {
-            System.out.println("Good evening!");
+            greeting = "Good evening!";
         }
+        return greeting;
     }
 }
