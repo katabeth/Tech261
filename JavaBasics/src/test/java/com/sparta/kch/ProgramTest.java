7d49fe2ed0d17dadef26323911b26f47b48fcfdd
Katherine <katabeth.ch@gmail.com>
2024-06-17 15:10:34 +0100

    Added JUnit and tested to see if it installed correctly

---
+++ JavaBasics/src/test/java/com/sparta/kch/ProgramTest.java
@@ -0,0 +1,11 @@
+package com.sparta.kch;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProgramTest {
+    @Test
+    public void basicTest(){
+        Assertions.assertTrue(5 == 6);
+    }
+}
