f9caddbefdcd0814872ee0a21d2d3fc2df2a6ada
Katherine <katabeth.ch@gmail.com>
2024-06-17 15:57:57 +0100

    Extra tests added to ProgramTest

--- JavaBasics/src/test/java/com/sparta/kch/ProgramTest.java
+++ JavaBasics/src/test/java/com/sparta/kch/ProgramTest.java
@@ -12,8 +12,9 @@ public class ProgramTest {
         Assertions.assertTrue(5 == 6);
     }
     */
+    //Evening number
     @Test
-    @DisplayName("Given a time of 21, then the greeting should be Good Evening") //is not always readable
+    @DisplayName("Given a time of 21, then the greeting should be Good evening!") //is not always readable
     void checkThat21GivesGoodEvening(){
         int time = 21;
         String expectedGreeting = "Good evening!";
@@ -22,6 +23,55 @@ public class ProgramTest {
         //Assertions.assertEquals("Good evening!", Program.getGreeting(21));
         // The variables don't need to be made for the code to function
     }
+    //Edge case of 12 -- Good afternoon!
+    @Test
+    @DisplayName("Given a time of 12, then the greeting should be Good afternoon!")
+    void checkThat12GivesGoodAfternoon(){
+        int time = 12;
+        String expectedGreeting = "Good afternoon!";
+        String actualGreeting = Program.getGreeting(time);
+        Assertions.assertEquals(expectedGreeting, actualGreeting);
+    }
+    //Time as a negative -- invalid time
+    @Test
+    @DisplayName("Given a time of -1, then the output should be Invalid Time")
+    void checkThatMinus1GivesErrorMessage(){
+        int time = -1;
+        String expectedGreeting = "Invalid time";
+        String actualGreeting = Program.getGreeting(time);
+        Assertions.assertEquals(expectedGreeting, actualGreeting);
+    }
+    //Time above 24 -- invalid time
+    @Test
+    @DisplayName("Given a time of greater than 24, then the output should be Invalid time")
+    void checkThat25GivesErrorMessage(){
+        int time = 25;
+        String expectedGreeting = "Invalid time";
+        String actualGreeting = Program.getGreeting(time);
+        Assertions.assertEquals(expectedGreeting, actualGreeting);
+    }
+    @Test
+    @DisplayName("Given a time of 18, then the output should be Good evening!")
+    void checkThat18GivesGoodEvening(){
+        int time = 18;
+        String expectedGreeting = "Good evening!";
+        String actualGreeting = Program.getGreeting(time);
+        Assertions.assertEquals(expectedGreeting, actualGreeting);
+    }
+    @Test
+    @DisplayName("Given a time of 0, then the output should be ...")
+    void checkThat0Gives(){
+        int time = 0;
+
+    }
+    //Between 0 - 5 -- Good night!
+    //Between 5 - 12 -- Good morning!
+    //Between 12 - 18 -- Good afternoon!
+    //Between 18 - 24 -- Good evening!
+    //All cases within 0-24 -- always give an output
+    //Output is always a value
+    //Time in words?
+    //Check if time is an int -- not usually needed
 
 
 }
