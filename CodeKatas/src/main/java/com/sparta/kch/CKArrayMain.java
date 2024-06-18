61b99deeb227683b47cd7438a3a2dbb48ef0da8b
Katherine <katabeth.ch@gmail.com>
2024-06-18 18:30:52 +0100

    Created CodeKata program and added JUnit dependencies

---
+++ CodeKatas/src/main/java/com/sparta/kch/CKArrayMain.java
@@ -0,0 +1,11 @@
+package com.sparta.kch;
+
+public class CKArrayMain {
+    public static void main(String[] args) {
+        //An array like this {1, 23, 4, 5} should return 12345 and an array like this {0, 1, 2, 3} should return 123
+
+    }
+    private static int getCalcVal(int[] numbers){
+
+    }
+}
