b3aad010372aa0b5b68dd7797205f0c65e65974c
Katherine <katabeth.ch@gmail.com>
2024-06-18 19:36:41 +0100

    Initial code built, 1st test complete and passed, second test built

--- CodeKatas/src/main/java/com/sparta/kch/CKArrayMain.java
+++ CodeKatas/src/main/java/com/sparta/kch/CKArrayMain.java
@@ -1,11 +1,19 @@
 package com.sparta.kch;
 
 public class CKArrayMain {
-    public static void main(String[] args) {
+
         //An array like this {1, 23, 4, 5} should return 12345 and an array like this {0, 1, 2, 3} should return 123
         //An empty Array should return 0
-    }
-    private static int getCalcVal(int[] numbers){
 
+    public static String getCalcVal(int[] numbers){
+        StringBuilder concatArray = new StringBuilder();
+
+        for(int element: numbers){         //iterates over an array
+            if (element != 0){
+                concatArray.append(element);
+            }
+
+        }
+        return concatArray.toString();
     }
 }
--- CodeKatas/src/test/java/com/sparta/kch/CKArrayTest.java
+++ CodeKatas/src/test/java/com/sparta/kch/CKArrayTest.java
@@ -8,21 +8,28 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CKArrayTest {
-    //Print numbers in order
-    //Number /3 = Fizz
-    //Number /5 = Buzz
-    //Number /5 & /3 = FizzBuzz
+    //An array like this {1, 23, 4, 5} should return 12345 and an array like this {0, 1, 2, 3} should return 123
+    //An empty Array should return 0
 
-    //Most simple test - Does 1 give 1?
+    //Most simple test - Does an array of 12345 give an output of 12345?
     @Test
-    @DisplayName("Input number is 1, then print number 1")
-    void InputOf1Returns1() {
+    @DisplayName("Input array is 1,2,3,4,5, then print number 12345")
+    void inputOf12345Returns12345() {
         //Arrange
-        //int input = 1;
-        //String expected = "1";
+        int[] input = {1,2,3,4,5};
+        String expected = "12345";
         //Act
-        //String actual = FizzBuzz.getFizzBuzzFrom(input);
+        String actual = CKArrayMain.getCalcVal(input);
         //Assert
+        Assertions.assertEquals(expected, actual);
+    }
+    //Array starting with 0
+    @Test
+    @DisplayName("Input array is 0,1,2,3, then print number 123")
+    void inputOf0123Returns123(){
+        int[] input = {0,1,2,3};
+        String expected = "123";
+        String actual = CKArrayMain.getCalcVal(input);
         Assertions.assertEquals(expected, actual);
     }
 }
