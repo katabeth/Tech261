dba15a645df924581c18160208edf2b175c88c7c
Katherine <katabeth.ch@gmail.com>
2024-06-18 19:53:12 +0100

    Tested for 1024 giving output of 1024. Test passed

--- CodeKatas/src/main/java/com/sparta/kch/CKArrayMain.java
+++ CodeKatas/src/main/java/com/sparta/kch/CKArrayMain.java
@@ -5,15 +5,12 @@ public class CKArrayMain {
         //An array like this {1, 23, 4, 5} should return 12345 and an array like this {0, 1, 2, 3} should return 123
         //An empty Array should return 0
 
-    public static String getCalcVal(int[] numbers){
+    public static int getCalcVal(int[] numbers){
         StringBuilder concatArray = new StringBuilder();
-
-        for(int element: numbers){         //iterates over an array
-            if (element != 0){
-                concatArray.append(element);
-            }
-
+        for (int element : numbers) {
+            concatArray.append(element);
         }
-        return concatArray.toString();
+
+        return Integer.parseInt(concatArray.toString());
     }
 }
--- CodeKatas/src/test/java/com/sparta/kch/CKArrayTest.java
+++ CodeKatas/src/test/java/com/sparta/kch/CKArrayTest.java
@@ -8,8 +8,14 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CKArrayTest {
-    //An array like this {1, 23, 4, 5} should return 12345 and an array like this {0, 1, 2, 3} should return 123
-    //An empty Array should return 0
+    /*
+    Given an array of integers, convert them to a single int made up of the digits
+    for example the array:
+        int[] numbers = {1, 0, 2, 4}
+        should return the int 1024.
+    An empty Array should return 0
+    Use TDD and ask if you need any requirements clarified
+    */
 
     //Most simple test - Does an array of 12345 give an output of 12345?
     @Test
@@ -17,9 +23,9 @@ public class CKArrayTest {
     void inputOf12345Returns12345() {
         //Arrange
         int[] input = {1,2,3,4,5};
-        String expected = "12345";
+        int expected = 12345;
         //Act
-        String actual = CKArrayMain.getCalcVal(input);
+        int actual = CKArrayMain.getCalcVal(input);
         //Assert
         Assertions.assertEquals(expected, actual);
     }
@@ -28,17 +34,19 @@ public class CKArrayTest {
     @DisplayName("Input array is 0,1,2,3, then print number 103")
     void inputOf0123Returns123(){
         int[] input = {0,1,2,3};
-        String expected = "123";
-        String actual = CKArrayMain.getCalcVal(input);
+        int expected = 123;
+        int actual = CKArrayMain.getCalcVal(input);
         Assertions.assertEquals(expected, actual);
     }
     //Checking if only first 0 is removed
     @Test
-    @DisplayName("Input array is 1,0,3,4 then print number 1034")
+    @DisplayName("Input array is 1,0,2,4 then print number 1024")
     void inputOf1034Returns1034(){
-        int[] input = {1,0,3,4};
-        String expected = "1034";
-        String actual = CKArrayMain.getCalcVal(input);
+        int[] input = {1,0,2,4};
+        int expected = 1024;
+        int actual = CKArrayMain.getCalcVal(input);
         Assertions.assertEquals(expected, actual);
     }
+    //Checking if empty array returns 0
+
 }
