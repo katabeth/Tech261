bed4d024f4241fa0c3620afd778b7f51edca445d
Katherine <katabeth.ch@gmail.com>
2024-06-18 15:44:44 +0100

    Created Class:Animal and declared variables

---
+++ AbstractClassesAndInterface/src/main/java/com/sparta/kch/Animal.java
@@ -0,0 +1,36 @@
+package com.sparta.kch;
+
+//Abstract deals with State and Behaviour
+public abstract class Animal {
+    private String name;
+    private String breed;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    
+
+
+}
