4a89e136e46533389a3a35be6d5d70d1efeca850
Katherine <katabeth.ch@gmail.com>
2024-06-18 15:40:49 +0100

    Getters and Setters

--- AbstractClassesAndInterface/src/main/java/com/sparta/kch/Dog.java
+++ AbstractClassesAndInterface/src/main/java/com/sparta/kch/Dog.java
@@ -10,6 +10,35 @@ public class Dog {
         this.breed = breed;
         this.age = age;
     }
+    //Setters and Getters
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public static void main(String[] args){
+        Dog max = new Dog("Max", "Terrier", 2);
+        Dog fido = new Dog("Fido", "Cockerspaniel", 5);
+        System.out.println(max.getName());
+    }
 
 }
