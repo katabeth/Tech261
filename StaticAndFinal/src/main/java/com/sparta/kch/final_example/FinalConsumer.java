08f17fc720312cc21c0de800a8077dffa1e2891a
Katherine <katabeth.ch@gmail.com>
2024-06-19 14:08:37 +0100

    Added examples of Final Classes, methods and variables

---
+++ StaticAndFinal/src/main/java/com/sparta/kch/final_example/FinalConsumer.java
@@ -0,0 +1,7 @@
+package com.sparta.kch.final_example;
+
+public class FinalConsumer extends FinalExample{
+    public final int getScore(){
+        return 10;
+    }
+}
--- StaticAndFinal/src/main/java/com/sparta/kch/final_example/FinalExample.java
+++ StaticAndFinal/src/main/java/com/sparta/kch/final_example/FinalExample.java
@@ -1,4 +1,11 @@
 package com.sparta.kch.final_example;
 
-public class FinalExample {
+public /*final*/ class FinalExample { //Final classes cannot be extended
+    private final int score = 100; //Final field cannot be changed, only referenced
+    public final int getScore(){ //Final method cannot be overridden
+        return score;
+    }
+    public void setScore(int score){
+        this.score = score;
+    }
 }
