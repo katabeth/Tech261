229acd7660d1b6c0f7d4e07b5045721a84d7e702
Katherine <katabeth.ch@gmail.com>
2024-06-19 13:47:17 +0100

    New Project of Static and Final with associated internal porjects and static example

---
+++ StaticAndFinal/src/main/java/com/sparta/kch/static_example/App.java
@@ -0,0 +1,14 @@
+package com.sparta.kch.static_example;
+
+public class App {
+    public static void main(String[] args) {
+        Spartan kat = new Spartan();
+        Spartan alex = new Spartan();
+
+        kat.setName("Kat");
+        alex.setName("Alex");
+
+        System.out.println(kat.getName());
+        System.out.println(alex.getName());
+    }
+}
---
+++ StaticAndFinal/src/main/java/com/sparta/kch/static_example/Spartan.java
@@ -0,0 +1,16 @@
+package com.sparta.kch.static_example;
+
+public class Spartan {
+    private static String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        Spartan.name = name;
+    }
+
+    public static void printSpartan(Spartan spartan){
+        System.out.println("Name: " + spartan.getName());
+    }
+
+}
