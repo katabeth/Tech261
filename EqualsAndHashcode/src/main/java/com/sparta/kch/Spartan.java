fcb9b3fb647a4f5b35c365c3d27a735437094c94
Katherine <katabeth.ch@gmail.com>
2024-06-19 15:11:33 +0100

    Made a hashcode equaliser

--- EqualsAndHashcode/src/main/java/com/sparta/kch/Spartan.java
+++ EqualsAndHashcode/src/main/java/com/sparta/kch/Spartan.java
@@ -21,17 +21,29 @@ public class Spartan {
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Spartan spartan = (Spartan) o;
+//        return Objects.equals(firstName, spartan.firstName) && Objects.equals(lastName, spartan.lastName);
+//    }
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(firstName, lastName);
+//    }
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Spartan spartan = (Spartan) o;
-        return Objects.equals(firstName, spartan.firstName) && Objects.equals(lastName, spartan.lastName);
+    public boolean equals(Object object){
+        if (this == object){
+            return true;
+        }
+        return (object instanceof Spartan otherSpartan)                 //Are these both Spartan Objects
+                && getFirstName().equals(otherSpartan.getFirstName())   //Do they have the same first name
+                && getLastName().equals(otherSpartan.getLastName());    //Do they have the same last name
     }
-
     @Override
     public int hashCode() {
-        return Objects.hash(firstName, lastName);
+       return Objects.hash(firstName, lastName);
     }
+
 }
