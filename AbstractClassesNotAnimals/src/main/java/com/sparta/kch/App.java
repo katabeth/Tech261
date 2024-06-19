91ba2d44589ae3d9eec61272b88373219b042b94
Katherine <katabeth.ch@gmail.com>
2024-06-19 13:37:29 +0100

    Spelling and Syntax fixes

--- AbstractClassesNotAnimals/src/main/java/com/sparta/kch/App.java
+++ AbstractClassesNotAnimals/src/main/java/com/sparta/kch/App.java
@@ -3,6 +3,6 @@ package com.sparta.kch;
 public class App {
     public static void main(String[] args){
         Trainee Kat = new JavaTrainee("Kat", 24, "katabeth.ch@gmail.com","07772201567","None" ,"Degree","None");
-        
+
     }
 }
