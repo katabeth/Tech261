09a7bb594558777f20a26554f2ef9df845f48718
Katherine <katabeth.ch@gmail.com>
2024-06-20 11:35:01 +0100

    First test written, code written, test failed but compiles

--- CodeKatas/src/main/java/com/sparta/kch/CKPalindromeMain.java
+++ CodeKatas/src/main/java/com/sparta/kch/CKPalindromeMain.java
@@ -1,4 +1,25 @@
 package com.sparta.kch;
 
+//Create a method which checks if a word is a palindrome.
+//
+//A palindrome is at minimum 3 characters long and is the same forwards as it is backwards
+//Spaces should not be ignored
+//The method should be case-insensitive
+//
+//For example: "Dad" is a palindrome, but "race car" is not.
+
 public class CKPalindromeMain {
+    public static boolean isPalindrome(String wordToCheck){
+        //Checks if same forward as is backwards
+        //Could break down in to char and compare
+        //Case insensitive
+        StringBuilder wordBackwards = new StringBuilder();
+        char[] charWordToCheck = wordToCheck.toLowerCase().toCharArray();
+        for (int element = wordToCheck.length()-1; element > 0; element--){
+            wordBackwards.append(charWordToCheck[element]);
+        }
+
+        return wordBackwards.toString().equals(wordToCheck);
+    }
+
 }
--- CodeKatas/src/test/java/com/sparta/kch/CKPalindromeTest.java
+++ CodeKatas/src/test/java/com/sparta/kch/CKPalindromeTest.java
@@ -1,4 +1,22 @@
 package com.sparta.kch;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 public class CKPalindromeTest {
+
+    @Test
+    @DisplayName("Input string /'Dad' Returns true")
+    void inputOfDadReturnsTrue() {
+        //Arrange
+        String input = "Dad";
+        boolean expected = true;
+        //Act
+        boolean actual = CKPalindromeMain.isPalindrome(input);
+        //Assert
+        Assertions.assertEquals(expected, actual);
+    }
+    
 }
