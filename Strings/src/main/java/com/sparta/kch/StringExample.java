99476e28f4fbfec40f396f1e1434361004b5ca86
Katherine <katabeth.ch@gmail.com>
2024-06-20 11:11:48 +0100

    Strings are immutable

--- Strings/src/main/java/com/sparta/kch/StringExample.java
+++ Strings/src/main/java/com/sparta/kch/StringExample.java
@@ -3,9 +3,16 @@ package com.sparta.kch;
 public class StringExample {
     public static void main(String[] args) {
         String string1 = "Hello";
-        string1 = string1.toUpperCase();
-        System.out.println(upperCase);
+        string1 = string1.toUpperCase(); //Immutable, so doesn't change the string saved but makes another version
+        System.out.println(string1.toCharArray());//
 
         String string = String.valueOf(100);
+
+        String name = "Kat";
+        System.out.println("Hello " + name); //Creates 3 strings
+
+        //String Builder
+        
+
     }
 }
