6a71edad32d4c909f1c2ba2042dc6fba9e9a8725
Katherine <katabeth.ch@gmail.com>
2024-06-20 10:54:55 +0100

    string.equals() vs ==

--- Strings/src/main/java/com/sparta/kch/App.java
+++ Strings/src/main/java/com/sparta/kch/App.java
@@ -14,8 +14,9 @@ public class App {
         System.out.println("");
         System.out.println(string1 == string2);     //True
         System.out.println(string1 == string3);     //False
+        System.out.println(string4 == string3);     //False
         System.out.println(string1.equals(string3));//True
-        
+
 
     }
 }
