53d44fc9941be8f404c0cf5d2b49a426721ed8c8
Katherine <katabeth.ch@gmail.com>
2024-06-20 10:46:01 +0100

    Made String project and App class to compare strings

---
+++ Strings/src/main/java/com/sparta/kch/App.java
@@ -0,0 +1,17 @@
+package com.sparta.kch;
+
+public class App {
+    public static void main(String[] args) {
+        String string1 = "Hello";
+        String string2 = "Hello";
+        String string3 = new String("Hello");
+        String string4 = new String("Hello");
+
+        System.out.println(string1.hashCode());
+        System.out.println(string2.hashCode());
+        System.out.println(string3.hashCode());
+        System.out.println(string4.hashCode());
+        System.out.println("");
+        System.out.println(string1 == string2);
+    }
+}
