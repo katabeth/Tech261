30fbcdbc6819271b95e49e1e04c3bf8f13c200ed
Katherine <katabeth.ch@gmail.com>
2024-06-26 15:36:58 +0100

    Created logic project and App class

---
+++ Logging/src/main/java/com/sparta/kch/App.java
@@ -0,0 +1,11 @@
+package com.sparta.kch;
+
+import java.util.logging.Logger;
+
+public class App {
+    private static final Logger logger = new Logger();
+
+    public static void main(String[] args) {
+        
+    }
+}
