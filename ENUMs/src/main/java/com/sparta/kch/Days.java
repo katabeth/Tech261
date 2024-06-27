7fe432e807bc9da414f73893666a2f0cdfb1b184
Katherine <katabeth.ch@gmail.com>
2024-06-27 11:44:40 +0100

    Created ENUMs project and Days class

---
+++ ENUMs/src/main/java/com/sparta/kch/Days.java
@@ -0,0 +1,14 @@
+package com.sparta.kch;
+
+public enum Days {
+    // Can create as many different objects but of a limited type
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+
+
+}
