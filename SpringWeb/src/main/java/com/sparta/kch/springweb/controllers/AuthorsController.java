26c609d92c8de82978f211bee406c51d02107604
Katherine <katabeth.ch@gmail.com>
2024-07-24 10:44:47 +0100

    Adding an entry form

--- SpringWeb/src/main/java/com/sparta/kch/springweb/controllers/AuthorsController.java
+++ SpringWeb/src/main/java/com/sparta/kch/springweb/controllers/AuthorsController.java
@@ -1,11 +1,16 @@
 package com.sparta.kch.springweb.controllers;
 
+import com.sparta.kch.springweb.entites.Author;
 import com.sparta.kch.springweb.repositories.AuthorRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/authors")
 public class AuthorsController {
 
     private final AuthorRepository authorRepository;
@@ -14,9 +19,20 @@ public class AuthorsController {
         this.authorRepository = authorRepository;
     }
 
-    @GetMapping("/authors")
-    public String authors(Model model) {
+    @GetMapping
+    public String authors(@RequestParam(required = false, defaultValue = "World") String msg, Model model) {
         model.addAttribute("authors", authorRepository.findAll());
+        model.addAttribute("msg", msg);
         return "authors/list";
+    }
+    @GetMapping("/create")
+    public String createAuthor(Model model) {
+        model.addAttribute("author", new Author());
+        return "authors/create";
+    }
+    @PostMapping("/create")
+    public String createAuthor(Author author) {
+        authorRepository.save(author);
+        return "redirect:/authors";
     }
 }
