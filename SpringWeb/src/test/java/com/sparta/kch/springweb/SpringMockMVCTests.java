2bb6da26b13d65acc35cf4f27c34ecfbdad4927d
Katherine <katabeth.ch@gmail.com>
2024-07-29 10:43:20 +0100

    Added duplicate tests with different syntax

--- SpringWeb/src/test/java/com/sparta/kch/springweb/SpringMockMVCTests.java
+++ SpringWeb/src/test/java/com/sparta/kch/springweb/SpringMockMVCTests.java
@@ -1,12 +1,14 @@
 package com.sparta.kch.springweb;
 
-import org.junit.jupiter.api.Test;
+import org.hamcrest.*;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,4 +23,19 @@ public class SpringMockMVCTests {
                 .andDo(MockMvcResultHandlers.print());
     }
 
+    @Test
+    public void testContentContainsWelcome() throws Exception {
+       String content = mockMvc.perform(MockMvcRequestBuilders.get("/"))
+               .andReturn()
+               .getResponse()
+               .getContentAsString();
+        MatcherAssert.assertThat(content, Matchers.containsString("Welcome"));
+    }
+
+    @Test
+    public void testWelcomePageExpectations() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("Welcome")));
+    }
 }
