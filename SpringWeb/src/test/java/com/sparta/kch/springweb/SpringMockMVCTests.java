8dda07c3d2dc79308fc6045b819c918a9bb63827
Katherine <katabeth.ch@gmail.com>
2024-07-29 10:32:38 +0100

    First MockMvcTest

--- SpringWeb/src/test/java/com/sparta/kch/springweb/SpringMockMVCTests.java
+++ SpringWeb/src/test/java/com/sparta/kch/springweb/SpringMockMVCTests.java
@@ -1,4 +1,24 @@
 package com.sparta.kch.springweb;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class SpringMockMVCTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testWelcomePage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
 }
