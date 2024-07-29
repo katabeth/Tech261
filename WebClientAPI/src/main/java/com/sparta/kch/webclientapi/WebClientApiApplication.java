6f5ad7548232b69ef692582601195450288ca1d0
Katherine <katabeth.ch@gmail.com>
2024-07-29 11:19:26 +0100

    New project WebClientAPI

---
+++ WebClientAPI/src/main/java/com/sparta/kch/webclientapi/WebClientApiApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.kch.webclientapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebClientApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebClientApiApplication.class, args);
+    }
+
+}
---
+++ WebClientAPI/src/test/java/com/sparta/kch/webclientapi/WebClientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.kch.webclientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
